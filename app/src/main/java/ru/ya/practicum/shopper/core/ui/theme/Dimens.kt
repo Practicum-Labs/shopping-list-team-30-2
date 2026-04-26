851ea254d24811d01a66e4fb84533eedc96ae94c
Roman Balan <rabalan83@gmail.com>
2026-04-26 18:25:46 +0300

    Вынос размеров в Dimens

--- app/src/main/java/ru/ya/practicum/shopper/core/ui/theme/Dimens.kt
+++ app/src/main/java/ru/ya/practicum/shopper/core/ui/theme/Dimens.kt
@@ -1,6 +1,17 @@
 package ru.ya.practicum.shopper.core.ui.theme
 
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 object Dimens {
     const val ZERO_FIVE = 0.5f
     const val AUTO_NAVIGATION_DELAY_MS = 3000L
+
+    val dp4 = 4.dp
+    val dp16 = 16.dp
+    val dp24 = 24.dp
+    val dp32 = 32.dp
+    val dp38 = 38.dp
+
+    val sp32 = 32.sp
 }
--- app/src/main/java/ru/ya/practicum/shopper/feature/onboard/components/OnboardLogo.kt
+++ app/src/main/java/ru/ya/practicum/shopper/feature/onboard/components/OnboardLogo.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import ru.ya.practicum.shopper.R
+import ru.ya.practicum.shopper.core.ui.theme.Dimens
 
 @Composable
 fun OnboardLogo(
@@ -15,6 +15,6 @@ fun OnboardLogo(
     Image(
         painter = painterResource(id = R.drawable.onb_logo),
         contentDescription = null,
-        modifier = modifier.padding(horizontal = 37.dp)
+        modifier = modifier.padding(horizontal = Dimens.dp38)
     )
 }
