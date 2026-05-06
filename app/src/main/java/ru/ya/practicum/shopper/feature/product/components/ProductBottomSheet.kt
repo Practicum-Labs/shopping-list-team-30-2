10afb2a29e0bc386f6c886d09539e38c3dca6bef
va-kadyrov <va.kadyrov@yandex.ru>
2026-05-06 17:49:08 +0300

    product bottom sheet ублажение CI

--- app/src/main/java/ru/ya/practicum/shopper/feature/product/components/ProductBottomSheet.kt
+++ app/src/main/java/ru/ya/practicum/shopper/feature/product/components/ProductBottomSheet.kt
@@ -1,6 +1,5 @@
 package ru.ya.practicum.shopper.feature.product.components
 
-import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,15 +14,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,15 +27,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import ru.ya.practicum.shopper.R
 import ru.ya.practicum.shopper.core.ui.theme.GreenLight
-import ru.ya.practicum.shopper.feature.main.components.IconView
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -132,33 +125,31 @@ private fun ProductBottomSheetString(
             contentDescription = null,
             modifier = Modifier.padding(top = 16.dp, bottom = 16.dp, start = 16.dp, end = 12.dp)
         )
-        if (substring == null)
+        if (substring == null) Text(
+            text = stringResource(stringRes),
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .weight(1f)
+                .height(56.dp)
+                .wrapContentHeight(Alignment.CenterVertically)
+        ) else Column(
+            modifier = Modifier
+                .weight(1f)
+                .height(56.dp)
+                .wrapContentHeight(Alignment.CenterVertically)
+        ) {
             Text(
                 text = stringResource(stringRes),
                 style = MaterialTheme.typography.bodyLarge,
                 color = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier
-                    .weight(1f)
-                    .height(56.dp)
-                    .wrapContentHeight(Alignment.CenterVertically)
-            ) else
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .height(56.dp)
-                    .wrapContentHeight(Alignment.CenterVertically)
-            ) {
-                Text(
-                    text = stringResource(stringRes),
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.onSurface,
-                )
-                Text(
-                    text = substring,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = GreenLight
-                )
-            }
+            )
+            Text(
+                text = substring,
+                style = MaterialTheme.typography.bodyMedium,
+                color = GreenLight
+            )
+        }
         if (iconEndRes != null)
             Icon(
                 painter = painterResource(iconEndRes),
